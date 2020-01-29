febb918e10c745654feb86faaa9dc1adcb1939f5
Amboid <i@amboid.ru>
2020-01-29 22:17:37 +0300

    copyright, codestyle

--- index.php
+++ index.php
@@ -1,26 +1,13 @@
 <?php
 /*
-Copyright 2020 Dmitry Bogdanov
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+Copyright © 2020 Dmitry Bogdanov
+Licensed under the Apache License, Version 2.0
 */
 
 
-
-//ini_set('error_reporting', E_ALL); ini_set('display_errors', 1);
-
-
+//ini_set('error_reporting', E_ALL); ini_set('display_errors', 1); // for debug
 require('./conf.php');
+
 
 
 if (!login()) {
@@ -63,15 +50,8 @@ Page::make();
 
 
 
-
-
-
-
-
-
-
-
 ////////////////////////////////////////////////////////////////////////
+
 
 
 function addNote() {
@@ -83,6 +63,7 @@ function addNote() {
     attachesUpload(DB::lastId());
     return true;
 }
+
 
 
 function attachesUpload($id) {
@@ -100,6 +81,8 @@ function attachesUpload($id) {
     return true;
 }
 
+
+
 function delDir($path) {
     if (!file_exists($path) || !is_dir($path)) return true;
     $dirHandle = opendir($path);
@@ -114,6 +97,8 @@ function delDir($path) {
     if (!rmdir($path)) throw new Exception("Error on delete dir $path", 20914532);
     return true;
 }
+
+
 
 function attachesList($id) {
     $dir = conf('ATTACH_DIR')."/$id";
@@ -150,7 +135,6 @@ function setStatus($id, $status) {
 
 
 
-
 function notesList() {
     $pg = isset($_GET['p']) ? (int)$_GET['p'] : 1;
     if ($pg<1) $pg = 1;
@@ -184,9 +168,6 @@ function notesList() {
 
 
 
-
-
-
 function login() {
     if (!isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'])) return false;
     
@@ -199,11 +180,6 @@ function login() {
     
     return false;
 }
-
-
-
-
-
 
 
 
@@ -275,7 +251,6 @@ class DB
 
 
 
-
 class Page
 {
     public static $title = 'MyNotes';
@@ -283,192 +258,191 @@ class Page
     
     public static function make() {
 ?><html>
-<head>
-    <title><?= static::$title ?></title>
-    
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-    <meta name="keywords" content="MyNotes">
-    <meta name="description" content="MyNotes">
-    <meta name="robots" content="noindex,nofollow">
-    
-
-    <style>
-        body {
-            background-color: #111;
-            color: #eeeeee;
-            font-family: "Segoe UI", "Trebuchet MS";
-            margin: 0;
-            display: flex;
-            flex-direction: column;
-            min-height: 100vh;
-        }
-
-        a   {color: #FFC093; text-decoration: none; border-bottom: 1px dotted #bbb; padding: 0;}
-        .r  {color: #FF5947;}
-        .g  {color: #A5FF7F;}
-        .b  {color: #7FC9FF;}
-        .o  {color: #FF6A00;}
-        .y  {color: #FFD800;}
-        .w  {color: #fff;}
+    <head>
+        <title><?= static::$title ?></title>
         
-        a:visited    {color: #FFC093; border-bottom: 1px solid #777;}
-        a.r:visited  {color: #FF5947;}
-        a.g:visited  {color: #A5FF7F;}
-        a.b:visited  {color: #7FC9FF;}
-        a.o:visited  {color: #FF6A00;}
-        a.y:visited  {color: #FFD800;}
-        a.w:visited  {color: #fff;}
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         
-        a:hover    {text-decoration: none; border-bottom: 1px dotted #bbb;}
+        <meta name="keywords" content="MyNotes">
+        <meta name="description" content="MyNotes">
+        <meta name="robots" content="noindex,nofollow">
         
-        hr {size: 1px; padding: 0px; margin: 0.5em 0; border-bottom: 1px dashed #666666; border-top: 1px dashed #333333; border-right: 0px solid #000000; border-left: 0px solid #000000;  text-align: center;}
 
-        .text-left   { text-align: left; }
-        .text-center { text-align: center; }
-        .text-right  { text-align: right; }
-
-        .container {
-            width: 100%;
-            margin: 0;
-            padding: 0;
-            background-color: #181818;
-            flex: 1 1 auto;
-        }
-
-        article { 
-            margin: 1.2rem 0;
-            background-color: #222;
-            padding: 0;
-            border-left: 4px solid #444;
-            overflow: scroll-x;
-        }
-        article:hover { background-color: #292929; }
-
-        article.st-1 { border-color: #444; }
-        article.st-2 { border-color: #0064D2; }
-        article.st-3 { border-color: #20BA5B; }
-        article.st-4 { border-color: #9A13ED; }
-        article.st-5 { border-color: #FE600D; }
-        article.st-0 { border-color: #333;  background-color: #181818;  opacity: 0.5; }
-        article.st-0 .d_text { color: #777;  font-size: 0.8em; }
-
-        a.st-1, a.st-1:visited, a.st-1:hover, a.st-1:focus, a.st-1:active { padding: 0 0.1em; color: #888; }
-        a.st-2, a.st-2:visited, a.st-2:hover, a.st-2:focus, a.st-2:active { padding: 0 0.1em; color: #0064D2; }
-        a.st-3, a.st-3:visited, a.st-3:hover, a.st-3:focus, a.st-3:active { padding: 0 0.1em; color: #20BA5B; }
-        a.st-4, a.st-4:visited, a.st-4:hover, a.st-4:focus, a.st-4:active { padding: 0 0.1em; color: #9A13ED; }
-        a.st-5, a.st-5:visited, a.st-5:hover, a.st-5:focus, a.st-5:active { padding: 0 0.1em; color: #FE600D; }
-
-
-
-        .d_head, .d_content {
-            display: flex;
-            flex-direction: row;
-        }
-        
-        .d_id, .d_dt, .d_controls {
-            padding: 0.1rem 0.4rem;
-        }
-        .d_id       { font-size: 0.8rem;  line-height: 1.2rem;  color: #aaa; }
-        .d_dt       { font-size: 0.8rem;  line-height: 1.2rem;  color: #777;  flex: 1 1 auto; }
-        .d_controls { font-size: 1.2rem;  line-height: 0.9rem;  color: #aaa; }
-        .d_controls a { border: 0px none;  opacity: 0.6; }
-        .d_controls a:hover { opacity: 1.0; }
-        
-        .d_attaches { padding: 0.2rem 1.5rem 0.3rem 0.4rem;  text-align: right;  font-family: Consolas; }
-        .d_text {
-            padding: 0.4rem 0.4rem;
-            flex: 1 1 auto;
-            color: #fff;
-        }
-        pre {
-            font-family: Consolas;
-            padding:0;
-            margin:0;
-            word-wrap: break-word;
-            white-space: pre-wrap;
-        }
-
-        .d_pages {
-            margin: 0.3rem 0;
-            padding: 0.2rem 0;
-            border-top: 1px solid #333;
-            border-bottom: 1px solid #333;
-        }
-
-        form { padding: 0.4rem; }
-        textarea {
-            box-sizing: border-box;
-            width: 100%;
-            height: 5rem;
-            border: 1px solid #666;
-            margin: 0;
-            padding: 0.4rem;
-            font-family: Consolas;
-            font-size: 1rem;
-            color: #fff;
-            background-color: #333;
-        }
-
-        .btn > input[type="file"] { display: none; }
-        .btn {
-            margin: 0.2em 0.1em;
-            display: inline-block;
-            font-weight: 400;
-            color: #fff;
-            text-align: center;
-            vertical-align: middle;
-            user-select: none;
-            background-color: #343a40;
-            border: 1px solid #343a40;
-            padding: 0.3em 0.6em;
-            font-size: 1em;
-            line-height: 1.5;
-        }
-        form .btn { font-size: 1.2rem; }
-        .btn:hover {
-            color: #fff;
-            background-color: #23272b;
-            border-color: #1d2124;
-        }
-        .btn:focus  { box-shadow: 0 0 0 0.2rem rgba(82, 88, 93, 0.5); }
-        .btn:active { color: #fff;  background-color: #1d2124;  border-color: #171a1d; }
-        .btn-o { color: #212529;  background-color: #ffc107;  border-color: #ffc107; }
-        .btn-o:hover { color: #212529;  background-color: #e0a800;  border-color: #d39e00; }
-        .btn-o:focus { box-shadow: 0 0 0 0.2rem rgba(222, 170, 12, 0.5); }
-        .btn-o.disabled { color: #212529;  background-color: #ffc107;  border-color: #ffc107; }
-        .btn-o:active { color: #212529;  background-color: #d39e00;  border-color: #c69500; }
-
-        article::-webkit-scrollbar,            textarea::-webkit-scrollbar             { width: 0.5rem; height: 0.5rem;}
-        article::-webkit-scrollbar-button,     textarea::-webkit-scrollbar-button      { background-color: #555; }
-        article::-webkit-scrollbar-track,      textarea::-webkit-scrollbar-track       { background-color: #999; }
-        article::-webkit-scrollbar-track-piece,textarea::-webkit-scrollbar-track-piece { background-color: #000; }
-        article::-webkit-scrollbar-thumb,      textarea::-webkit-scrollbar-thumb       { height: 4rem;  background-color: #666; }
-        article::-webkit-scrollbar-corner,     textarea::-webkit-scrollbar-corner      { background-color: #999; }
-        article::-webkit-resizer,              textarea::-webkit-resizer               { background-color: #666; }
-
-
-        @media (min-width: 992px) {
-            .container {
-                max-width: 960px;
-                padding-right: 1rem;
-                padding-left: 1rem;
-                margin: 0 auto;
-                border-left: 1px solid #222;
-                border-right: 1px solid #242424;
+        <style>
+            body {
+                background-color: #111;
+                color: #eeeeee;
+                font-family: "Segoe UI", "Trebuchet MS";
+                margin: 0;
+                display: flex;
+                flex-direction: column;
+                min-height: 100vh;
             }
-            form { padding: 0.4rem 0; }
-        }
 
-  </style>
-</head>
+            a   {color: #FFC093; text-decoration: none; border-bottom: 1px dotted #bbb; padding: 0;}
+            .r  {color: #FF5947;}
+            .g  {color: #A5FF7F;}
+            .b  {color: #7FC9FF;}
+            .o  {color: #FF6A00;}
+            .y  {color: #FFD800;}
+            .w  {color: #fff;}
 
-<body>
-    <div class='container'>
-        <?= static::$main ?>
-    </div>
-</body>
+            a:visited    {color: #FFC093; border-bottom: 1px solid #777;}
+            a.r:visited  {color: #FF5947;}
+            a.g:visited  {color: #A5FF7F;}
+            a.b:visited  {color: #7FC9FF;}
+            a.o:visited  {color: #FF6A00;}
+            a.y:visited  {color: #FFD800;}
+            a.w:visited  {color: #fff;}
+
+            a:hover    {text-decoration: none; border-bottom: 1px dotted #bbb;}
+
+            hr {size: 1px; padding: 0px; margin: 0.5em 0; border-bottom: 1px dashed #666666; border-top: 1px dashed #333333; border-right: 0px solid #000000; border-left: 0px solid #000000;  text-align: center;}
+
+            .text-left   { text-align: left; }
+            .text-center { text-align: center; }
+            .text-right  { text-align: right; }
+
+            .container {
+                width: 100%;
+                margin: 0;
+                padding: 0;
+                background-color: #181818;
+                flex: 1 1 auto;
+            }
+
+            article { 
+                margin: 1.2rem 0;
+                background-color: #222;
+                padding: 0;
+                border-left: 4px solid #444;
+                overflow: scroll-x;
+            }
+            article:hover { background-color: #292929; }
+
+            article.st-1 { border-color: #444; }
+            article.st-2 { border-color: #0064D2; }
+            article.st-3 { border-color: #20BA5B; }
+            article.st-4 { border-color: #9A13ED; }
+            article.st-5 { border-color: #FE600D; }
+            article.st-0 { border-color: #333;  background-color: #181818;  opacity: 0.5; }
+            article.st-0 .d_text { color: #777;  font-size: 0.8em; }
+
+            a.st-1, a.st-1:visited, a.st-1:hover, a.st-1:focus, a.st-1:active { padding: 0 0.1em; color: #888; }
+            a.st-2, a.st-2:visited, a.st-2:hover, a.st-2:focus, a.st-2:active { padding: 0 0.1em; color: #0064D2; }
+            a.st-3, a.st-3:visited, a.st-3:hover, a.st-3:focus, a.st-3:active { padding: 0 0.1em; color: #20BA5B; }
+            a.st-4, a.st-4:visited, a.st-4:hover, a.st-4:focus, a.st-4:active { padding: 0 0.1em; color: #9A13ED; }
+            a.st-5, a.st-5:visited, a.st-5:hover, a.st-5:focus, a.st-5:active { padding: 0 0.1em; color: #FE600D; }
+
+
+
+            .d_head, .d_content {
+                display: flex;
+                flex-direction: row;
+            }
+
+            .d_id, .d_dt, .d_controls {
+                padding: 0.1rem 0.4rem;
+            }
+            .d_id       { font-size: 0.8rem;  line-height: 1.2rem;  color: #aaa; }
+            .d_dt       { font-size: 0.8rem;  line-height: 1.2rem;  color: #777;  flex: 1 1 auto; }
+            .d_controls { font-size: 1.2rem;  line-height: 0.9rem;  color: #aaa; }
+            .d_controls a { border: 0px none;  opacity: 0.6; }
+            .d_controls a:hover { opacity: 1.0; }
+
+            .d_attaches { padding: 0.2rem 1.5rem 0.3rem 0.4rem;  text-align: right;  font-family: Consolas; }
+            .d_text {
+                padding: 0.4rem 0.4rem;
+                flex: 1 1 auto;
+                color: #fff;
+            }
+            pre {
+                font-family: Consolas;
+                padding:0;
+                margin:0;
+                word-wrap: break-word;
+                white-space: pre-wrap;
+            }
+
+            .d_pages {
+                margin: 0.3rem 0;
+                padding: 0.2rem 0;
+                border-top: 1px solid #333;
+                border-bottom: 1px solid #333;
+            }
+
+            form { padding: 0.4rem; }
+            textarea {
+                box-sizing: border-box;
+                width: 100%;
+                height: 5rem;
+                border: 1px solid #666;
+                margin: 0;
+                padding: 0.4rem;
+                font-family: Consolas;
+                font-size: 1rem;
+                color: #fff;
+                background-color: #333;
+            }
+
+            .btn > input[type="file"] { display: none; }
+            .btn {
+                margin: 0.2em 0.1em;
+                display: inline-block;
+                font-weight: 400;
+                color: #fff;
+                text-align: center;
+                vertical-align: middle;
+                user-select: none;
+                background-color: #343a40;
+                border: 1px solid #343a40;
+                padding: 0.3em 0.6em;
+                font-size: 1em;
+                line-height: 1.5;
+            }
+            form .btn { font-size: 1.2rem; }
+            .btn:hover {
+                color: #fff;
+                background-color: #23272b;
+                border-color: #1d2124;
+            }
+            .btn:focus  { box-shadow: 0 0 0 0.2rem rgba(82, 88, 93, 0.5); }
+            .btn:active { color: #fff;  background-color: #1d2124;  border-color: #171a1d; }
+            .btn-o { color: #212529;  background-color: #ffc107;  border-color: #ffc107; }
+            .btn-o:hover { color: #212529;  background-color: #e0a800;  border-color: #d39e00; }
+            .btn-o:focus { box-shadow: 0 0 0 0.2rem rgba(222, 170, 12, 0.5); }
+            .btn-o.disabled { color: #212529;  background-color: #ffc107;  border-color: #ffc107; }
+            .btn-o:active { color: #212529;  background-color: #d39e00;  border-color: #c69500; }
+
+            article::-webkit-scrollbar,            textarea::-webkit-scrollbar             { width: 0.5rem; height: 0.5rem;}
+            article::-webkit-scrollbar-button,     textarea::-webkit-scrollbar-button      { background-color: #555; }
+            article::-webkit-scrollbar-track,      textarea::-webkit-scrollbar-track       { background-color: #999; }
+            article::-webkit-scrollbar-track-piece,textarea::-webkit-scrollbar-track-piece { background-color: #000; }
+            article::-webkit-scrollbar-thumb,      textarea::-webkit-scrollbar-thumb       { height: 4rem;  background-color: #666; }
+            article::-webkit-scrollbar-corner,     textarea::-webkit-scrollbar-corner      { background-color: #999; }
+            article::-webkit-resizer,              textarea::-webkit-resizer               { background-color: #666; }
+
+            @media (min-width: 992px) {
+                .container {
+                    max-width: 960px;
+                    padding-right: 1rem;
+                    padding-left: 1rem;
+                    margin: 0 auto;
+                    border-left: 1px solid #222;
+                    border-right: 1px solid #242424;
+                }
+                form { padding: 0.4rem 0; }
+            }
+
+      </style>
+    </head>
+    
+    <body>
+        <div class='container'>
+            <?= static::$main ?>
+        </div>
+    </body>
 </html><?php
         exit();
     }
@@ -534,7 +508,7 @@ class Page
     }
 
 
-    
+
     public static function pages($current, $all) {
         if ($all <= 1) return '';
         $ret = '';
